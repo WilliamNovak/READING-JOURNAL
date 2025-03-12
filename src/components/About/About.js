58479b87819c48f4fec537534d2bcf36e3174589
William Novak <novakwilliamr@gmail.com>
2025-03-11 22:19:34 -0300

    create about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,7 +2,10 @@ import React from "react";
 
 const About = () => {
     return (
-        <h1>Sobre</h1>
+        <div>
+            <h1>Sobre</h1>
+            <p>Esta é uma aplicação para um CRUD de um Reading Journal. Este projeto foi elaborado na disciplina Desenvolvimento de Sistemas Frontend do Curso de Graduação Online da PUCRS.</p>
+        </div>
     );
 }
   
