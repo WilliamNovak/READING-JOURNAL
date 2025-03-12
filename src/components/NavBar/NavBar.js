117058950ba488234b1dfd4da480b5f6be2415a6
William Novak <novakwilliamr@gmail.com>
2025-03-11 21:18:17 -0300

    add NavBar component

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,23 @@
+
+const NavBar = () => {
+    return (
+        <nav>
+          <ul>
+            <li>
+              <a href=''>Página Inicial</a>
+            </li>
+            <li>
+              <a href=''>Sobre</a>
+            </li>
+            <li>
+              <a href=''>Lista de livros</a>
+            </li>
+            <li>
+              <a href=''>Cadastrar</a>
+            </li>
+          </ul>
+        </nav>
+    )
+}
+
+export default NavBar;
