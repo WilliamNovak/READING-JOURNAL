7b39fc5d14cca7c1b79f5109150423d60e90ecde
William Novak <novakwilliamr@gmail.com>
2025-03-17 18:02:32 -0300

    created books object

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
-import BookList from "./components/BookList/BookList"
-import BookForm from "./components/BookForm/BookForm"
+import BookList from "./components/BookList/BookList";
+import BookForm from "./components/BookForm/BookForm";
+import myBooks from "./assets/books";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useState } from "react";
 import "./App.css";
 
 export default function App() {
-  const [books, setBooks] = useState([
-    {id: 1, title: 'Livro 1', author: 'Autor 1', genre: 'Terror', date: '2020-01-10'},
-    {id: 2, title: 'Livro 2', author: 'Autor 2', genre: 'Ficção', date: '2018-06-09'},
-    {id: 3, title: 'Livro 3', author: 'Autor 3', genre: 'Romance', date: '2021-11-27'}
-  ]);
+  const [books, setBooks] = useState(myBooks);
 
   const [bookId, setBookId] = useState(4);
 
