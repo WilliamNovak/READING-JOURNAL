6b06a932acbee67a020f24b6a1a94b997472d34c
William Novak <novakwilliamr@gmail.com>
2025-03-27 01:01:39 -0300

    style about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,13 +1,42 @@
 import React from "react";
+import { Typography, Container, Box } from "@mui/material";
 
 // Página ilustrativa com informacoes sobre o sistema
 const About = () => {
-    return (
-        <div>
-            <h1>Sobre</h1>
-            <p style={{width: "800px", margin: "auto"}}>Esta é uma aplicação para um CRUD de um Reading Journal. Este projeto foi elaborado na disciplina Desenvolvimento de Sistemas Frontend do Curso de Graduação Online da PUCRS.</p>
-        </div>
-    );
-}
-  
+  return (
+    <Container maxWidth="md">
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          minHeight: "60vh"
+        }}
+      >
+        <Typography variant="h4" gutterBottom>
+          Sobre
+        </Typography>
+        <Typography variant="body1"
+          sx={{
+            maxWidth: "600px",
+            marginTop: "0.5rem"
+          }}
+        >
+          Esta é uma aplicação para um CRUD de um Reading Journal.
+        </Typography>
+        <Typography variant="body1"
+          sx={{
+            maxWidth: "600px",
+            marginTop: "0.5rem"
+          }}
+        >
+          Este projeto foi elaborado na disciplina Desenvolvimento de Sistemas
+          Frontend do Curso de Graduação Online da PUCRS.
+        </Typography>
+      </Box>
+    </Container>
+  );
+};
+
 export default About;
