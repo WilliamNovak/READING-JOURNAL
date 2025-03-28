5a804a43e93ce3ba66a7478fd028e47257360972
William Novak <novakwilliamr@gmail.com>
2025-03-27 22:24:21 -0300

    delete file books.js

--- src/assets/books.js
+++
@@ -1,8 +0,0 @@
-// Objeto com livros ilustrativos inicialmente cadastrados
-const myBooks = [
-    {id: 1, title: 'Livro 1', author: 'Autor 1', genre: 'Terror', date: '2020-01-10'},
-    {id: 2, title: 'Livro 2', author: 'Autor 2', genre: 'Ficção', date: '2018-06-09'},
-    {id: 3, title: 'Livro 3', author: 'Autor 3', genre: 'Romance', date: '2021-11-27'}
-]
-
-export default myBooks;
